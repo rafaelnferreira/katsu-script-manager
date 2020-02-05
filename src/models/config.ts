692752c908006f8c9cb26ef9ecf44555e7ca4521
silvio_piccolo <silvio_piccolo@calypso.com>
2020-02-05 17:33:19 +0000

    added exec job endpoint
    Change-Id: I37160366860fb2e1b5e64b33ad7f0046c18e4df6

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,8 +1,10 @@
 export class Config {
-    readonly dockerComposeFolder = __dirname + '/../../config/docker-compose/';
-    readonly scriptsFolder = process.env['SCRIPT_FOLDER'] || __dirname + '/../../config/scripts/';
-    readonly tempFolder = __dirname + '/../../~tmp/';
-    readonly logsFolder = this.tempFolder + '/logs/';
+    readonly dockerComposeFolder = __dirname + '/../../../config/docker-compose/';
+    readonly scriptsFolder = __dirname + '/../../../config/scripts/';
+    readonly tempFolder = __dirname + '/../../../~tmp-docker/';
+    readonly dockerLogsFolder = this.tempFolder + '/logs/';
+    readonly scriptsLogsFolder = __dirname + '/../../../scripts-logs/';
+
     commandsList: Map<string, string>;
     environmentVariables: string;
 }
