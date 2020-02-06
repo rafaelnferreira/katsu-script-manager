0351c9c9b0c288d29edf70f19518e895b11336b3
silvio_piccolo <silvio_piccolo@calypso.com>
2020-02-06 16:53:59 +0000

    added script folder
    Change-Id: If92bfc3e9863c2f0af28c55cbe61c696ea0035b0

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,6 +1,6 @@
 export class Config {
     readonly dockerComposeFolder = __dirname + '/../../../config/docker-compose/';
-    readonly scriptsFolder = __dirname + '/../../../config/scripts/';
+    readonly scriptsFolder = process.env['SCRIPT_FOLDER'] || __dirname + '/../../config/scripts/';
     readonly tempFolder = __dirname + '/../../../~tmp-docker/';
     readonly dockerLogsFolder = this.tempFolder + '/logs/';
     readonly scriptsLogsFolder = __dirname + '/../../../scripts-logs/';
