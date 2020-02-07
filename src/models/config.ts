6b3fd4afa0bfc2a72207c84f28f86de9d6a69580
silvio_piccolo <silvio_piccolo@calypso.com>
2020-02-07 10:36:58 +0000

    refactored to get rid of script manager
    Change-Id: Id4cd5ed5ab9e251ddbab16fd32df42ccb27f45db

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,9 +1,7 @@
 export class Config {
-    readonly dockerComposeFolder = __dirname + '/../../../config/docker-compose/';
-    readonly scriptsFolder = process.env['SCRIPT_FOLDER'] || __dirname + '/../../config/scripts/';
-    readonly tempFolder = __dirname + '/../../../~tmp-docker/';
+    readonly dockerComposeFolder = __dirname + '/../../config/docker-compose/';
+    readonly tempFolder = __dirname + '/../../~tmp-docker/';
     readonly dockerLogsFolder = this.tempFolder + '/logs/';
-    readonly scriptsLogsFolder = __dirname + '/../../../scripts-logs/';
 
     commandsList: Map<string, string>;
     environmentVariables: string;
