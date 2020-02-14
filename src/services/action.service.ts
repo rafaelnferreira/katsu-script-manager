43b6e490039088e007dd99524b21df9b7b832c7e
silvio_piccolo <silvio_piccolo@calypso.com>
2020-02-14 16:26:31 +0000

    added logging
    Change-Id: I68917eb71e95c629d4adc1f5649f1e5bdb79dd30

--- src/services/action.service.ts
+++ src/services/action.service.ts
@@ -4,22 +4,24 @@ import { Util } from '../util/util';
 import {JobsInput} from '../types/jobs-input';
 import jobsConfig from '../../config/jobs/jobs.config.json'; 
 import moment from 'moment';
+import {logger} from './../app';
 
 export class ActionService {
 
     constructor(private applicationStatus: ApplicationStatus){}
 
     runScript(scriptName: string, args: string[], logsName?: string ): boolean {
+        logger.debug('Trying to run script: ' + scriptName);
+
         const matchedScriptFile = this.applicationStatus.scriptsNames.find((s) => s === scriptName);
 
         if(matchedScriptFile) {
+            logger.debug('Script found');
             const scriptProc = Util.execFile(`${this.applicationStatus.config.scriptsFolder}/${scriptName}`, args, this.applicationStatus.config.scriptsFolder);
        
             // organize logs in daily folders 
             const todayFolder = moment().format('YYYYMMDD')
-            console.log(todayFolder)
-
-
+        
             if (!fs.existsSync(this.applicationStatus.config.scriptsLogsFolder + todayFolder)) {
                 fs.mkdirSync(this.applicationStatus.config.scriptsLogsFolder + todayFolder);
             }
@@ -33,11 +35,21 @@ export class ActionService {
 
             return true;
         }
+
+        logger.debug('No script found');
         return false;
     }
 
     executeJob(job: JobsInput) {
+        logger.debug('Trying to execute job: ' + job.jobName + ' on branch: ' + job.branch);
         const applicableJobs = jobsConfig.filter(c => c.jobName === job.jobName && c.branch === job.branch);
-        applicableJobs.forEach(job => this.runScript(job.scriptName, job.args, job.name));
+
+        if(applicableJobs.length > 0) {
+            logger.debug('Found jobs number: ' + applicableJobs.length);
+            applicableJobs.forEach(job => this.runScript(job.scriptName, job.args, job.name));
+        } else {
+            logger.debug('No applicable jobs found');
+        }
+        
     }
 }
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,4 +1,4 @@
 export class Config {
-    readonly scriptsFolder = process.env['SCRIPT_FOLDER'] || __dirname + '/../../config/scripts/';
+    readonly scriptsFolder = process.env['SCRIPT_FOLDER'] || __dirname + '/../../../config/scripts/';
     readonly scriptsLogsFolder = __dirname + '/../../../scripts-logs/';
 }
