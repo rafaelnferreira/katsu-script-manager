46a6527e338c8d0a352ce663acafeebd763e59aa
silvio_piccolo <silvio_piccolo@calypso.com>
2020-03-04 16:08:32 +0000

    added get, save and delete job endpoints
    Change-Id: Id58d5ff751e7445a54d8967e4a025a26f0bd0dbc

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-    readonly scriptsFolder = process.env['SCRIPT_FOLDER'] || __dirname + '/../../../config/scripts/';
-    readonly scriptsLogsFolder = __dirname + '/../../../scripts-logs/';
+    readonly scriptsFolder = process.env['SCRIPT_FOLDER'] || __dirname + '/../../config/scripts/';
+    readonly scriptsLogsFolder = __dirname + '/../../scripts-logs/';
     readonly delayedExecution = +process.env['DELAYED_EXECUTION'] || 1000;
 }
